eb33fdc49e036357f2e36875153633a041ebb687
wansook0316 <wansook0316@gmail.com>
2022-08-29 12:50:54 +0900

    Create: 기본 파일들 생성

---
+++ Composite/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Composite
+//
+//  Created by Choiwansik on 2022/08/29.
+//
+
+import Foundation
---
+++ Composite/Folder.swift
@@ -0,0 +1,8 @@
+//
+//  Folder.swift
+//  Composite
+//
+//  Created by Choiwansik on 2022/08/29.
+//
+
+import Foundation
